f063359816a674c86c4a86de98e00171118cccd1
EC2 Default User <yurika428521@gmail.com>
2024-04-15 00:29:22 +0900

    課題8-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
+
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
   public function index(Post $post)
 {
-   return view('posts.index')->with(['posts'=>$post->getPaginateByLimit(1)]);
+   return view('posts.index')->with(['posts'=>$post->getPaginateByLimit()]);
 }
-    
+    public function show(Post $post)
+{
+  return view('posts/show')->with(['post'=> $post]);
+}
 }
 ?>
